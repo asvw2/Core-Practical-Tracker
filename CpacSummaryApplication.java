92db2a7e6d8f1cff62e0463841053a2185bb7ff0
asvw2 <asvw2@cantab.net>
2025-10-29 18:54:21 +0000

    Create CpacSummaryApplication as Spring Boot app

---
+++ CpacSummaryApplication.java
@@ -0,0 +1,12 @@
+src/main/java/com/example/cpacsummary/CpacSummaryApplication.java
+package com.example.cpacsummary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpacSummaryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CpacSummaryApplication.class, args);
+    }
+}
